30990ef87f340e1cf66695e206ba63e8796bcd64
Christian M <christian.mouchet@epfl.ch>
2017-11-27 12:02:18 +0100

    Finished combatant extraction

--- project/processing/features/combatants.py
+++ project/processing/features/combatants.py
@@ -1,6 +1,9 @@
 import mwparserfromhell as mw
+import re
 from utils import get_templates
+from collections import OrderedDict
 
+ACRONYMS_PAT = re.compile("\b(?:[A-Z]\.?){2,}")
 
 def get_combatants(infobox):
     return {"combatant_%s" % n: get_combatant(infobox.get("combatant%s" % n)) for n in range(1, 4)}
@@ -12,15 +15,18 @@ def get_combatant(value):
 
     wt = mw.parse(value.strip())
 
-    links = [(l, l.title.startswith(("File:", "Image:"))) for l in wt.filter_wikilinks()]
-    if links:
-        return [str(l.text) if l.text else str(l.title) for l, isImage in links if not isImage]
+    links = wt.filter_wikilinks()
+    links_e = [l.__strip__() for l in links if not l.title.startswith(("File:", "Image:"))]
+    for l in links:
+        wt.remove(l)
 
-    flagicons = get_templates(wt, ["flagicon", "flagicon image","flagcountry", "flag", "Flagdeco", "flagu", "flagcountry", "Flag icon", "army", "navy"])
-    if flagicons:
-        return [str(i.params[0]) for i in flagicons]
+    templates = get_templates(wt, ["flagicon", "flagicon image","flagcountry", "flag", "Flagdeco", "flagu", "flagcountry", "Flag icon", "army", "navy"])
+    templates_e = [str(i.params[0]) for i in templates]
+    for t in templates:
+        wt.remove(t)
 
-    if len(wt.filter()) == 1:
-        return [wt.strip_code()]
+    non_ref_text = " ".join([str(t) for t in wt.filter(recursive=False, matches=lambda node: not node.startswith(("<ref", )))])
 
-    return
+    acronyms = ACRONYMS_PAT.findall(non_ref_text)
+
+    return list(OrderedDict.fromkeys(links_e + templates_e + acronyms))  # Removes duplicates
