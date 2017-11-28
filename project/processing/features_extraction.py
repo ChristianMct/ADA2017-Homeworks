e67da151b022575de2e9c42f9c517fc6be60ddab
Christian M <christian.mouchet@epfl.ch>
2017-11-28 14:26:07 +0100

    added date and coordinate parsing to feature extraction

--- project/processing/features_extraction.py
+++ project/processing/features_extraction.py
@@ -13,17 +13,19 @@ def extract_battle_features(line):
         return None
 
     features = dict()
-    for module in [combatants, ]:
+    for module in [combatants, date, coordinates]:
         features.update(module.get_features(battle))
 
     features.update(results.get_features(battle, {cl: features.get(cl, []) for cl in combatants.COMBATANT_LISTS}))
 
     return json.dumps(features) if features else None
 
-if len(sys.argv) != 3:
+if len(sys.argv) < 3:
     print("Incorrect number of argument passed to the job")
     sys.exit(1)
 
 filename_in = sys.argv[1]
 filename_out = sys.argv[2]
-process_file_by_line(open(filename_in, mode="r"), open(filename_out, mode="w"), extract_battle_features)
+limit = int(sys.argv[3]) if len(sys.argv) >= 4 else -1
+
+process_file_by_line(open(filename_in, mode="r"), open(filename_out, mode="w"), extract_battle_features, limit)
